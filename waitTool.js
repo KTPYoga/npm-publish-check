2cbccfe30d377c3ddac5d919c74249376d511e1e
yogaraj selvam <yogaraj.selvam@kaplan.com>
2017-02-14 12:03:05 -0500

    changed wait page and version

--- waitTool.js
+++ waitTool.js
@@ -1,5 +1,7 @@
-module.exports = {
-    printMessage: function (){
-        console.log("Message from node");
-    }
-}
+var waitPage = function(){
+    this.printMessage= function(){
+        console.log('Message from node');
+    };
+   
+};
+module.exports = new waitPage;
